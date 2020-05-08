c2263012b061faecc4f485a80d9b79c66e7def21
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-08 06:27:52 +0530

    [shaiksha]add. some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* FormsModule */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+/* Angular Flex Layout */
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,12 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
