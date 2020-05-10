e5229f23a479f5bdf98ac731853d22a60a5e3f5a
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-05-11 04:05:59 +0530

    [ChandrakalaKirasur]pagination in front using ngx paginator is working fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
 /* FormsModule */
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 /* Angular Flex Layout */
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { NgxPaginationModule } from "ngx-pagination";
 
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { BooksComponent } from "./components/books/books.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { BooksComponent } from './components/books/books.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-import { MatInputModule, MatSnackBarModule, MatNavList} from "@angular/material";
+import {
+  MatInputModule,
+  MatSnackBarModule,
+  MatNavList,
+} from "@angular/material";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { MatSidenavModule } from "@angular/material/sidenav";
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from "@angular/material/list";
 import { MatMenuModule } from "@angular/material/menu";
 
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ViewCartComponent } from './components/view-cart/view-cart.component';
-import { PaginationComponent } from './components/pagination/pagination.component';
-import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { GetbooksComponent } from './components/getbooks/getbooks.component';
-
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ViewCartComponent } from "./components/view-cart/view-cart.component";
+import { PaginationComponent } from "./components/pagination/pagination.component";
+import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+import { GetbooksComponent } from "./components/getbooks/getbooks.component";
+import { SortBooksPipe } from "./pipes/sort-books.pipe";
 
 @NgModule({
   declarations: [
@@ -50,6 +52,7 @@ import { GetbooksComponent } from './components/getbooks/getbooks.component';
     PaginationComponent,
     OrderSuccessComponent,
     GetbooksComponent,
+    SortBooksPipe,
   ],
   imports: [
     BrowserModule,
@@ -58,6 +61,7 @@ import { GetbooksComponent } from './components/getbooks/getbooks.component';
     ReactiveFormsModule,
     FormsModule,
     FlexLayoutModule,
+    NgxPaginationModule,
     MatFormFieldModule,
     MatIconModule,
     MatCardModule,
@@ -73,9 +77,8 @@ import { GetbooksComponent } from './components/getbooks/getbooks.component';
     MatInputModule,
     MatSnackBarModule,
     BrowserAnimationsModule,
-
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
