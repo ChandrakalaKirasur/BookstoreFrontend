6d608471e8c551bc18f7b32b895b9c9e2caf2e37
iamshaiksha <iamshaiksha2019@gmail.com>
2020-05-11 08:38:08 +0530

    [refractor].changes done of ui and validations matform field border colour

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
+import { Login } from 'src/app/models/login';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+   /**
+   * login object about consist of 
+   * 1)email
+   * 2)password feilds
+   */
+  login: Login = new Login("", "");
+  loginForm: FormGroup;
+  token: string;
+  email = new FormControl(this.login.email, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]);
+  password = new FormControl(this.login.password, [Validators.required, Validators.minLength(8)])
 
   constructor() { }
 
   ngOnInit() {
   }
-
+/**
+   * Email validation
+   */
+  getErrorEmail() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  /**
+   * Password Validation
+   */
+  getErrorPassword() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+     this.password.hasError('password') ? 'Min 6 Elements' : '';
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  user: User = new User();
+
+  name = new FormControl(this.user.name, [Validators.required, Validators.minLength(8), Validators.pattern('[a-zA-Z ]*')]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl(this.user.password, [Validators.required, Validators.minLength(8), Validators.maxLength(15)])
+  mobile = new FormControl(this.user.phoneNumber, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]);
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  nameValidation() {
+    return this.name.hasError('required') ? 'must required' : '';
+  }
+
+  MobileNumber() {
+    return this.mobile.hasError('required') ? 'must required' : '';
+  }
+
+  emailValidation() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+      this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getErrorPassword() {
+    return this.password.hasError('required') ? 'You must enter a value' :
+      this.password.hasError('password') ? 'Min 6 Elements' : '';
+  }
+
 
 }
