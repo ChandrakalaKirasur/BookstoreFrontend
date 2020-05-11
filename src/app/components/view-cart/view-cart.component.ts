3b65a20a9f73c54fd357199dfeccd91fa70aa0c6
saikiranmsk <msaikiran0728@gmail.com>
2020-05-11 21:35:24 +0530

    [Sai] refractor. added the validations in the address fields

--- src/app/components/view-cart/view-cart.component.ts
+++ src/app/components/view-cart/view-cart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
+import { NgxSpinnerService } from "ngx-spinner";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Login } from "src/app/models/login";
 
 @Component({
   selector: "app-view-cart",
@@ -7,17 +10,78 @@ import { FormControl, Validators } from "@angular/forms";
   styleUrls: ["./view-cart.component.scss"],
 })
 export class ViewCartComponent implements OnInit {
-  email = new FormControl("", [Validators.required, Validators.email]);
+  images = [{}, {}, {}, {}];
+  image: "assets/images/Image 11@2x.png";
+  login: Login = new Login("", "");
 
-  emailValidation() {
-    return this.email.hasError("required")
-      ? ""
-      : this.email.hasError("email")
-      ? ""
-      : "";
+  name = new FormControl([
+    Validators.required,
+    Validators.minLength(4),
+    Validators.pattern("[a-zA-Z ]*"),
+  ]);
+  mobile = new FormControl([
+    Validators.required,
+    Validators.minLength(10),
+    Validators.maxLength(10),
+  ]);
+  pincode = new FormControl([
+    Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(6),
+  ]);
+
+  locality = new FormControl([
+    Validators.required,
+    Validators.minLength(10),
+    Validators.pattern("[a-zA-Z ]*"),
+  ]);
+  address = new FormControl([
+    Validators.required,
+    Validators.minLength(8),
+    Validators.pattern("[a-zA-Z ]*"),
+  ]);
+  city = new FormControl([
+    Validators.required,
+    Validators.minLength(10),
+    Validators.pattern("[a-zA-Z ]*"),
+  ]);
+  landmark = new FormControl([
+    Validators.required,
+    Validators.minLength(10),
+    Validators.pattern("[a-zA-Z ]*"),
+  ]);
+
+  nameValidation() {
+    return this.name.hasError("required") ? "" : "";
+  }
+  phoneNumber() {
+    return this.mobile.hasError("required") ? "" : "";
+  }
+  pincodeValidation() {
+    return this.name.hasError("required") ? "" : "";
+  }
+  localityValidation() {
+    return this.locality.hasError("required") ? "" : "";
+  }
+  addressValidation() {
+    return this.address.hasError("required") ? "" : "";
+  }
+  cityValidation() {
+    return this.city.hasError("required") ? "" : "";
+  }
+  landmarkValidation() {
+    return this.landmark.hasError("required") ? "" : "";
   }
 
-  constructor() {}
+  // password = new FormControl(this.login.password, [
+  //   Validators.required,
+  //   Validators.minLength(7)
+  // ]);
+  constructor(
+    private spinner: NgxSpinnerService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
@@ -28,13 +92,29 @@ export class ViewCartComponent implements OnInit {
     this.fields = true;
   }
 
+  showSpinner = false;
   open2: boolean;
+
   onOpen2() {
-    this.open2 = true;
+    this.spinner.show();
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.spinner.hide();
+      this.open2 = true;
+    }, 2000);
     this.fields = false;
+    console.log(this.login.email + "*****************");
   }
   onEdit() {
     this.fields = true;
     this.open2 = false;
+  }
+  onCheckOut() {
+    this.spinner.show();
+    this.showSpinner = true;
+    setTimeout(() => {
+      this.spinner.hide();
+      this.router.navigate(["/ordersucess"]);
+    }, 2000);
   }
 }
