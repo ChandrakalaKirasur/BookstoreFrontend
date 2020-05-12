6a3d2942ea927478948a887973e36dec6ecf45fc
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-05-12 13:10:41 +0530

    [ChandrakalaKirasur]added http service methods

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,25 +1,24 @@
 import { Observable } from "rxjs";
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root",
 })
 export class HttpService {
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private http: HttpClient) {}
   public postMethod(url: string, body: any, options: any): Observable<any> {
-    return this._httpClient.post(url, body, options);
+    return this.http.post(url, body, options);
   }
   public getMethod(url: string, options: any): Observable<any> {
-    return this._httpClient.get(url, options);
+    return this.http.get(url, options);
   }
   public putMethod(url: string, body: any, options: any): Observable<any> {
-    return this._httpClient.put(url, body, options);
+    return this.http.put(url, body, options);
   }
   public deleteMethod(url: string, options: any): Observable<any> {
-    return this._httpClient.delete(url, options);
+    return this.http.delete(url, options);
   }
   public httpOptions = {
     headers: new HttpHeaders({
@@ -28,12 +27,11 @@ export class HttpService {
     }),
   };
   baseurl = environment.baseUrl;
-  constructor(private http: HttpClient) { }
 
-  public postRequest(url :any, data: any ):any{
-    return this.http.post("http://localhost:8080/seller/" + url,data);
+  public postRequest(url: any, data: any): any {
+    return this.http.post("http://localhost:8080/seller/" + url, data);
   }
-  public putRequestForget(url,data){
-    return this.http.post("http://localhost:8080/seller/"+ url,data,);
+  public putRequestForget(url, data) {
+    return this.http.post("http://localhost:8080/seller/" + url, data);
   }
 }
