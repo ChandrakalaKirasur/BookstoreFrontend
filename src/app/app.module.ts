6ab96df70664e36bdb1bc1e0dc99fdfa9e56f2c6
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-05-12 10:11:26 +0530

    [ChandrakalaKirasur]footer added to other pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
   MatInputModule,
   MatSnackBarModule,
+  MatTooltipModule,
   MatNavList,
 } from "@angular/material";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -88,6 +89,7 @@ import { NgxSpinnerModule } from "ngx-spinner";
     MatProgressBarModule,
     MatRadioModule,
     NgxSpinnerModule,
+    MatTooltipModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
