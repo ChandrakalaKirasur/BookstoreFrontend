ce51182e758ae2bfe0b7c3550b8d0202082b7184
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-05-12 11:43:51 +0530

    [ChandrakalaKirasur]added http service methods

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class HttpService {
-  constructor() {}
+  constructor(private _httpClient: HttpClient) {}
+  public postMethod(url: string, body: any, options: any): Observable<any> {
+    return this._httpClient.post(url, body, options);
+  }
+  public getMethod(url: string, options: any): Observable<any> {
+    return this._httpClient.get(url, options);
+  }
+  public putMethod(url: string, body: any, options: any): Observable<any> {
+    return this._httpClient.put(url, body, options);
+  }
+  public deleteMethod(url: string, options: any): Observable<any> {
+    return this._httpClient.delete(url, options);
+  }
+  public httpOptions = {
+    headers: new HttpHeaders({
+      "content-type": "application/json",
+      token: localStorage.getItem("token"),
+    }),
+  };
 }
