e51d1f5675951a9f44ea10d6993193f8900f6ac7
ChandrakalaKirasur <59432828+ChandrakalaKirasur@users.noreply.github.com>
2020-05-30 02:14:35 +0530

    [ChandrakalaKirasur]rating ui working fine

--- src/app/components/ratingreview/ratingreview.component.ts
+++ src/app/components/ratingreview/ratingreview.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Book } from "src/app/models/book";
+import { BookService } from "src/app/service/book.service";
+import { Router } from "@angular/router";
+import { MatSnackBar, MatDialog } from "@angular/material";
+import { LoginComponent } from "../login/login.component";
 
 @Component({
-  selector: 'app-ratingreview',
-  templateUrl: './ratingreview.component.html',
-  styleUrls: ['./ratingreview.component.scss']
+  selector: "app-ratingreview",
+  templateUrl: "./ratingreview.component.html",
+  styleUrls: ["./ratingreview.component.scss"],
 })
 export class RatingreviewComponent implements OnInit {
-
-  constructor() { }
+  book: Book;
+  bookId: any;
+  visible: boolean;
+  constructor(
+    private bookService: BookService,
+    private router: Router,
+    private _matSnackBar: MatSnackBar,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
+    this.bookId = localStorage.getItem("bookId");
+    this.getBookById();
+    if (localStorage.getItem("token") != null) {
+      this.visible = true;
+      this.isAddedToCart();
+      this.isAddedToWishList();
+    }
   }
-
+  getBookById() {
+    this.bookService.getBookById(this.bookId).subscribe((response: any) => {
+      this.book = response["obj"];
+      this.book.isListed = false;
+      this.book.isAdded = false;
+    });
+  }
+  addToCart() {
+    if (this.visible) {
+      this.bookService.addToCart(this.bookId).subscribe((response: any) => {
+        console.log(response["obj"]);
+        this.book.isAdded = response.obj;
+        this._matSnackBar.open("Book added to cart", "ok", {
+          duration: 1000,
+        });
+      });
+    } else {
+      const dialogRef = this.dialog.open(LoginComponent);
+      dialogRef.afterClosed().subscribe((result) => {
+        window.location.reload();
+      });
+      this._matSnackBar.open("please login", "ok", {
+        duration: 1000,
+      });
+    }
+  }
+  //adding book to wish list if user login
+  addToWishlist() {
+    if (this.visible) {
+      this.bookService.addToWishList(this.bookId).subscribe((response: any) => {
+        console.log(response["obj"]);
+        this.book.isListed = response["obj"];
+        this._matSnackBar.open("Book added to wishlist", "ok", {
+          duration: 1000,
+        });
+      });
+    } else {
+      const dialogRef = this.dialog.open(LoginComponent);
+      dialogRef.afterClosed().subscribe((result) => {
+        window.location.reload();
+      });
+      this._matSnackBar.open("please login", "ok", {
+        duration: 1000,
+      });
+    }
+  }
+  //getting boolean as a output and finding whether book is already
+  isAddedToCart() {
+    this.bookService.isAddedTocart(this.bookId).subscribe((response: any) => {
+      this.book.isAdded = response["obj"];
+    });
+  }
+  isAddedToWishList() {
+    this.bookService
+      .isAddedToWishList(this.bookId)
+      .subscribe((response: any) => {
+        this.book.isListed = response["obj"];
+      });
+  }
 }
